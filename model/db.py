be660481eb890ca5dfc950e05637b76478d1add6
emilim <emilpyhtond@gmail.com>
2024-08-31 18:00:17 +0900

    dbモデルを使える形にした。

--- model/db.py
+++ model/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy.sql import extract  # extract関数をインポート
+from datetime import datetime
 
 # ベースクラスの作成
 Base = declarative_base()
@@ -43,18 +44,97 @@ class FitData(Base):
     user = relationship("User", back_populates="fit_data")
 
 # AWS RDSの接続情報
+DATABASE_TYPE = ''
+DB_HOST = ''
+DB_NAME = ''
+DB_USER = ''
+DB_PASSWORD = ''
+DB_PORT = ''
 
 # 接続URLを生成
 DATABASE_URL = f'{DATABASE_TYPE}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
 # データベースとの接続設定
-engine = create_engine(DATABASE_URL)  # SQLiteを使用していますが、他のDBも指定可能
+engine = create_engine(DATABASE_URL)
 
 # セッションの作成
 Session = sessionmaker(bind=engine)
-session = Session()
 
-# 特定のユーザーのデータを取得
-user_data = session.query(FitData).filter_by(user_id=1).all()
+class DatabaseClient:
+    def __init__(self):
+        self.session = Session()
 
-for data in user_data:
-    print(data.datetime, data.steps, data.distance, data.weight, data.fat)
+    def get_all_users(self):
+        try:
+            users = self.session.query(User).all()
+            return users
+        finally:
+            self.session.close()
+
+    def get_user_fit_data(self, user_id):
+        try:
+            return self.session.query(FitData).filter_by(user_id=user_id).all()
+        finally:
+            self.session.close()
+
+    def add_user(self, user_name, group_id=None):
+        try:
+            new_user = User(user_name=user_name, group_id=group_id)
+            self.session.add(new_user)
+            self.session.commit()
+        except:
+            self.session.rollback()
+            raise
+        finally:
+            self.session.close()
+
+    def get_users_by_group(self, group_id):
+        try:
+            users = self.session.query(User).filter_by(group_id=group_id).all()
+            return users
+        finally:
+            self.session.close()
+
+    def add_fit_data(self, user_id, steps, distance, weight, fat):
+        try:
+            new_fit_data = FitData(
+                user_id=user_id,
+                datetime=datetime.now(),
+                steps=steps,
+                distance=distance,
+                weight=weight,
+                fat=fat
+            )
+            self.session.add(new_fit_data)
+            self.session.commit()
+        except:
+            self.session.rollback()
+            raise
+        finally:
+            self.session.close()
+    
+    def get_user_fit_data_for_current_month(self, user_id):
+        """指定されたuser_idの今月のfit_dataを取得"""
+        try:
+            # 現在の年と月
+            now = datetime.now()
+            current_year = now.year
+            current_month = now.month
+
+            # 指定したユーザーの今月のデータを取得
+            fit_data = self.session.query(FitData).filter(
+                FitData.user_id == user_id,
+                extract('year', FitData.datetime) == current_year,
+                extract('month', FitData.datetime) == current_month
+            ).all()
+            return fit_data
+        finally:
+            self.session.close()
+            
+# 使用例
+if __name__ == "__main__":
+    db_client = DatabaseClient()
+    user_fit_data_this_month = db_client.get_user_fit_data_for_current_month(5)
+    for data in user_fit_data_this_month:
+        print(f"Date: {data.datetime}, Steps: {data.steps}, Distance: {data.distance}, Weight: {data.weight}, Fat: {data.fat}")
+
