b768337644263906edcda2150904619c03606bdf
emilim <emilpyhtond@gmail.com>
2024-10-05 14:01:29 +0000

    groupとuserを追加するsqlを追加した

---
+++ db/insert_user.sql
@@ -0,0 +1,7 @@
+INSERT INTO tbl_group (group_id, group_name) VALUES (1, 'test_group');
+
+INSERT INTO tbl_user (user_name, group_id) VALUES ('user1', 1);
+INSERT INTO tbl_user (user_name, group_id) VALUES ('user2', 1);
+INSERT INTO tbl_user (user_name, group_id) VALUES ('user3', 1);
+INSERT INTO tbl_user (user_name, group_id) VALUES ('user4', 1);
+
