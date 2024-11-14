a3c8b214c611ff961a9da0503ade291d5efd378b
emilim <emilpyhtond@gmail.com>
2024-11-14 09:59:56 +0000

    歩数補正機能を追加した＆コードを簡略化した

--- uap.py
+++ uap.py
@@ -7,17 +7,15 @@ def main():
     # db: INSTANCE GENERATION
     db_client = DatabaseClient()
     
-    # db: group_idを指定して所属する全てのユーザのuser_idを取得
-    user_ids = []
+    # db: group_idを指定して所属する全てのユーザ情報を取得
     users = db_client.get_users_by_group(1)
-    for user in users:
-        user_ids.append(user.user_id)
         
     # gf: 各ユーザのfit_dataを取得してリスト化
     fit_data_list = []
-    for user_id in user_ids:
-        gf_client = GoogleFitClient(user_id)
+    for user in users:
+        gf_client = GoogleFitClient(user.user_id)
         gf_data = gf_client.fetch_combined_data()
+        gf_data['steps'] *= user.steps_coefficient #steps補正
         fit_data_list.append(gf_data)
     
     # db: weight,fatが未更新の場合は直近のデータを引き継ぎ
--- update_fit_data.py
+++ update_fit_data.py
@@ -6,17 +6,17 @@ def main():
     # db: INSTANCE GENERATION
     db_client = DatabaseClient()
     
-    # db: group_idを指定して所属する全てのユーザのuser_idを取得
-    user_ids = []
+    # db: group_idを指定して所属する全てのユーザ情報を取得
     users = db_client.get_users_by_group(1)
-    for user in users:
-        user_ids.append(user.user_id)
     
     # gf: 過去のfitデータをまとめて取得
     gf_data_list = []
-    for user_id in user_ids:
-        gf_client = GoogleFitClient(user_id)
-        gf_data = gf_client.fetch_past_data(7)
+    for user in users:
+        gf_client = GoogleFitClient(user.user_id)
+        gf_data = gf_client.fetch_past_data(5)
+        for gfd in gf_data:
+            gfd['steps'] *= user.steps_coefficient #steps補正
+        print(gf_data)
         gf_data_list += gf_data #　一つのリストにまとめる
     
     # db: update
